82ec5025cd2a942119ab6d231ff54bd6dddee393
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-12-21 21:05:37 +0200

    update controller to show port for running app

--- src/main/java/com/photoapp/discovery/api/users/ui/controller/UsersController.java
+++ src/main/java/com/photoapp/discovery/api/users/ui/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.photoapp.discovery.api.users.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    private final Environment environment;
+
+    @Autowired
+    public UsersController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working!";
+        return "Working on port: " + environment.getProperty("local.server.port");
     }
 }
